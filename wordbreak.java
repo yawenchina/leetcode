536ade7fe2de0c320e0467e464c90b7e46ee60fd
yawenchina <yawenchina@126.com>
2014-11-10 23:26:46 -0800

    work break

--- wordbreak.java
+++ wordbreak.java
@@ -1,21 +1,23 @@
 public class Solution {
     
     public boolean wordBreak(String s, Set<String> dict) {
-       boolean[] check = new boolean[s.length()+1];
-       check[0] = true;
-       for(int i = 0;i < s.length();i++){
-           if(!check[i])
-            continue;
-            for(String one : dict){
-                int len = one.length();
-                if(len+i > s.length()){
-                   continue;
-               }
-                if(s.substring(i,i+len).equals(one)){
-                    check[i+len] = true;;
-                }    
+        boolean[] check = new boolean[s.length()+1];
+        check[0] = true;
+        for(int i = 0; i < s.length(); i++){
+            if(!check[i]){
+                continue;
             }
-       }
-       return check[s.length()];
+            for(String m : dict){
+                int len = m.length();
+                if(i+len > s.length()){
+                    continue;
+                }
+                if(s.substring(i,i+len).equals(m)){
+                    check[len+i] = true;
+                }
+            }
+        }
+        return check[s.length()];
+      
     }
 }
