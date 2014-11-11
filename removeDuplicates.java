66c3c2c9c1ee9224979ace82320f9432e1fd0d3f
yawenchina <yawenchina@126.com>
2014-11-10 23:27:04 -0800

    remove duplicates from array 2

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,31 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
-        }
-        ListNode result = head;
-        ListNode one = head;
-        ListNode move = one.next;
-        while(move != null){
-            if(one.val != move.val){
-                one.next = move;
-                one = one.next;
-                
-            }
-            move = move.next;
-        }
-        one.next = null;
-        return result;
+    public int removeDuplicates(int[] A) {
+         int count = 0;  
+        for (int i=2; i<A.length; ++i) {  
+        if (A[i] == A[i-count-1] && A[i] == A[i-count-2]) { // skip duplicates  
+            ++count;  
+        } else if (count > 0) { // copy over non-duplicates  
+            A[i-count] = A[i];  
+        }  
+        }  
+        return A.length - count;
     }
 }
